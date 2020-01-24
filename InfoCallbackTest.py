d0b1c5f57fbc1d44cf3e709d6f8034402d83afc6
unknown <15oa16@queensu.ca>
2020-01-24 11:32:59 -0500

    separated info callback by train and test

---
+++ InfoCallbackTest.py
@@ -0,0 +1,17 @@
+import tensorflow as tf
+from trainingMetrics import save_wins
+
+
+class InfoCallbackTest(tf.keras.callbacks.Callback):
+    def __init__(self):
+        self.player_win = False
+
+    def on_step_end(self, step, logs=None):
+        if (logs['info'])['matches_won'] == 2:
+            self.player_win = True
+        elif (logs['info'])['matches_won'] == 0 and self.player_win is True:
+            save_wins(self.player_win, "test")
+            self.player_win = False
+
+    def on_episode_end(self, episode, logs=None):
+        save_wins(self.player_win, "test")
